922fdbb17497b4ccad8a54417943009fb879e15b
d_wisarut <d.wisarut@hotmail.com>
2026-02-05 04:01:43 +0700

    refactor: refactor and separate the quotes from component

---
+++ src/components/quotes.ts
@@ -0,0 +1,14 @@
+const quotes: string[] = [
+  "C is weird and quirky for those who doesn't learn to program.",
+  "I like to look at one or two random quotes each morning. It can be a good exercise for journaling prompts.",
+  "Most of the world’s biggest problems are because of somebody trying to find a simple answer.",
+  "The gulf between competence and incompetence amplifies the more competence is needed.",
+  "Anger has a honey tip, but a poison source.",
+  "If you are to have a great kingdom, rule over yourself!",
+  "It must be produced and discharged and used up in order to exist at all.",
+  "The real problem of humanity is the following: we have paleolithic emotions; medieval institutions; and god-like technology.",
+  "Time is the king of all men, he is their parent and their grave, and gives them what he will and not what they crave.",
+  "Do you want to know who you are? Don't ask. Act! Action will delineate and define you.",
+];
+
+export default quotes;
